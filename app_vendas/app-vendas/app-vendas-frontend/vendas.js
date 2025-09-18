b79689fd3155685d010f40250aca940a31c97155
Luan Clemente Costa <79475114+LuanClemente@users.noreply.github.com>
2025-09-18 17:24:19 -0300

    Update vendas.js

--- app_vendas/app-vendas/app-vendas-frontend/vendas.js
+++ app_vendas/app-vendas/app-vendas-frontend/vendas.js
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         searchTimeout = setTimeout(async () => {
             try {
-                const response = await fetch(`https://app-vendas-front-production.up.railway.app/?busca=${termo}`, {
+                const response = await fetch(`window.process.env.API_URL?busca=${termo}`, {
                     headers: { 'Authorization': `Bearer ${token}` }
                 });
                 if (!response.ok) throw new Error('Falha na busca');
